9efdd53266007192e793cad47bcc5324a92deeba
ShaquilleONeal <shaquilleonealdiesel34@gmail.com>
2024-11-10 15:24:44 +0800

    Commit 10 November

---
+++ src/Administrator/Admin.java
@@ -0,0 +1,13 @@
+package Administrator;
+public class Admin extends User {
+	int id;
+	String name;
+	String gender;
+	int age;
+	public Admin() {
+		this.id=0;
+		this.name="";
+		this.gender="";
+		this.age=0;
+	}
+}
