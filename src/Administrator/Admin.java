b1e500e30b2ad27cfe18971233d56f9175128bab
ShaquilleONeal <shaquilleonealdiesel34@gmail.com>
2024-11-11 17:35:59 +0800

    11 Nov

--- src/Administrator/Admin.java
+++ src/Administrator/Admin.java
@@ -1,11 +1,14 @@
 package Administrator;
 import User.Model.User;
+import java.lang.String;
+
 public class Admin extends User {
 	int id;
 	String name;
 	String gender;
 	int age;
-	public Admin() {
+	public Admin(String hospitalID, String passwordHash, String role) {
+		super(hospitalID,passwordHash,role);
 		this.id=0;
 		this.name="";
 		this.gender="";
